76abac61615318058bbc0fccbd4471ef930cf9fe
james-ng-dev <james.ng.dev@gmail.com>
2022-06-10 16:14:20 +0700

    First commit

---
+++ components/bio/Bio.js
@@ -0,0 +1,29 @@
+import styles from './bio.module.scss'
+
+export function Bio({ className }) {
+  return (
+    <div className={styles.bio}>
+      <div className={styles.profile}>
+        <img
+          src="/static/images/avatar.png"
+          alt="Profile Image"
+          className="rounded-full "
+          style={{
+            width: '268px',
+          }}
+        />
+        <div className={styles.hi}>
+          <h2 className={styles.black}>Hi.</h2>
+          <h2 className={styles.white}>Hi.</h2>
+        </div>
+      </div>
+      <div className={styles.description}>
+        <p>
+          I'm Nguyen Tri
+          <br />
+          I'm <span className="font-bold">developer</span> 👨🏻‍💻
+        </p>
+      </div>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,87 @@
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/data/siteMetadata'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+import formatDate from '@/lib/utils/formatDate'
+
+import NewsletterForm from '@/components/NewsletterForm'
+import { Bio } from '@/components/bio'
+
+const MAX_DISPLAY = 5
+
+export async function getStaticProps() {
+  const posts = await getAllFilesFrontMatter('blog')
+
+  return { props: { posts } }
+}
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <div className="dark:divide-gray-700">
+        <Bio />
+        <ul className="my-10 grid gap-10 lg:grid-cols-2">
+          {!posts.length && 'No posts found.'}
+          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+            const { slug, date, title, summary, tags } = frontMatter
+            return (
+              <li key={slug} className="bg-auto p-10 py-12 shadow-lg">
+                <article>
+                  <div className="">
+                    <div className="">
+                      <div className="">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                          <div className="my-5 flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div>
+                        </div>
+                        <span className="my-5 text-sm">
+                          <time dateTime={date}>{formatDate(date)}</time>
+                        </span>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
+                        </div>
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blog/${slug}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {posts.length > MAX_DISPLAY && (
+        <div className="flex justify-center text-base font-medium leading-6">
+          <Link
+            href="/blog"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label="all posts"
+          >
+            All Posts
+          </Link>
+        </div>
+      )}
+    </>
+  )
+}
